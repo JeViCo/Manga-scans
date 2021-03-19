000a20e9c8a448633d27a8bb66c14a07a057a2e2
JeViCo <jevico-official@outlook.com>
2021-03-19 16:22:28 +0300

    Добавлена иконка, добавлена склейка + обрезка сканов

---
+++ Тест/Tacit.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+web = input ( 'Введите ссылку на главу:\n' )
+    
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+response = requests.get ( web, headers=headers )
+html = bs ( response.text, 'html.parser' )
+
+
+path = os.getcwd ( ) + '\\scans\\{}\\'.format ( 'Тест' )
+if not os.path.exists ( os.path.dirname ( path ) ):
+    os.makedirs ( os.path.dirname ( path ) )
+
+print ( 'Началось скачивание сканов!' )
+
+num = 0
+while True:
+    img = html.find ( 'img', { 'id': 'content_image_{}'.format ( num ) } )
+    if img is None:
+        break
+    #src
+    img_src = img[ 'src' ].strip ( )
+    img_file = img_src.split ( '/' )[ -1 ]
+    #очень длинное название
+    part = img_file.split ( '_' )
+    img_file = part[ -2 ] + '_' + part[ -1 ]
+    
+    #save
+    r = requests.get ( img_src, stream=True, headers=headers )
+    
+    with open ( path + img_file, 'wb') as outfile:
+        outfile.write ( r.content )
+    
+    #remove
+    img.decompose()
+    num += 1
+
+input ( 'Все изображения сохранены!\n' )
---
+++ Тест/TacitSplitter.py
@@ -0,0 +1,54 @@
+import cv2
+import os.path
+import numpy as np
+import matplotlib.pyplot as plt
+
+gFolderEng = 'Tacit_Scans'
+gFolderRu = 'Тест'
+gMask = 'IMAG01_{}'
+
+def getDivs ( h ):
+    lst = [ ]
+    for i in range(  h - 1, 1, -1 ):
+        if ( h % i == 0 ):
+            lst += [ i ]
+    return lst
+
+def processScan ( img ):
+    num = 0
+    height = img.shape[ 0 ]
+
+    path = os.path.join ( os.getcwd ( ), 'scans', gFolderEng )
+    if not os.path.exists ( path ):
+        os.makedirs ( path )
+    
+    print ( 'Формирование сканов началось!\nВыберите высоту сканов. Рекомендуемые значения:' )
+    print ( *getDivs ( height ), sep=", " )
+    
+    pageSize = int ( input ( ) )
+    comp = int ( input ( 'Выберите качество изображений (0-100):\n' ) )
+    
+    for y in range ( 0, height, pageSize ):
+        tilePath = os.path.join ( path, '{:03d}.jpg'.format ( num ) )
+        cv2.imwrite ( tilePath, img[ y: y+pageSize, 0:1024 ].copy ( ), [cv2.IMWRITE_JPEG_QUALITY, comp] )
+        num += 1
+    print ( 'Формирование сканов завершено!' )
+    input ( '\nФрагменты были сохранены по пути:\n{}\n'.format ( path ) )
+
+def mergeScans ( ):    
+    num = 1
+    height = 0
+    imgs = [ ]
+
+    print ( 'Склейка сканов началась!' )    
+
+    while True:
+        path = os.path.join ( os.getcwd ( ), 'scans', gFolderRu, gMask.format ( num ) + '.jpg');
+        if not os.path.isfile ( path ):
+            print ( 'Склейка сканов завершена!\n' )
+            processScan ( cv2.vconcat ( imgs ) )
+            break
+        imgs += [ plt.imread ( path )[..., ::-1] ]
+        num += 1
+
+mergeScans ( ) # start
